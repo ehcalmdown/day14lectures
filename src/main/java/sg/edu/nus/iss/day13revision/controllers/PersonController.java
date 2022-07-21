f15fc1d6b9747a01b2f64b0638ce51b44b076d5e
ehcalmdown <96929724+ehcalmdown@users.noreply.github.com>
2022-07-21 14:25:04 +0800

    modified htmls and models

--- src/main/java/sg/edu/nus/iss/day13revision/controllers/PersonController.java
+++ src/main/java/sg/edu/nus/iss/day13revision/controllers/PersonController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -53,6 +54,23 @@ public class PersonController {
     public String showAddPerson(Model model){
         PersonForm pForm = new PersonForm();
         model.addAttribute("personForm", pForm);
+
+        return "addPerson";
+    }
+    @RequestMapping(value="/addPerson", method = RequestMethod.POST)
+    public String savePerson(Model model, @ModelAttribute("personForm") PersonForm personForm){
+
+        String fName = personForm.getFirstName();
+        String lName = personForm.getLastName();
+
+        if(fName != null && fName.length() > 0 && lName !=null && lName.length()>0){
+            PersonModel newPerson = new PersonModel(fName, lName);
+            perSvc.addPerson(newPerson);
+
+            return "redirect:/personList";
+
+        }
+        model.addAttribute("errorMessage", errorMessage);
 
         return "addPerson";
     }
